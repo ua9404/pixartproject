d540bd0a4fbca6e812a4618a4361617bdcf6fa5c
umara <ua9404@rit.edu>
2021-09-21 14:55:54 +0400

    Implemented 50% of init: - Speed is initialized - X and Y coordinates are initialized

--- pixart.py
+++ pixart.py
@@ -2,12 +2,23 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import turtle
 
+XMAX = 200
+XMIN = -XMAX
+YMAX = 200
+YMIN = -YMAX
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def init():
+    turtle.reset()
+    turtle.goto(XMIN, YMAX)
+
+    turtle.speed(0)
+    return
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
---
+++ pixart_test.py
@@ -0,0 +1,12 @@
+import pixart
+import turtle
+
+def test_init():
+    pixart.init()
+
+    s=turtle.speed()
+    assert(s==0)
+
+    x=turtle.xcor();
+    assert(x==-200)
+
